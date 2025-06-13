56546d6869bea66f6a3ab696f5bfaeaeb27564bc
Jadon Jung <jadonjung3@gmail.com>
2025-06-12 17:56:48 -0700

    Initial commit of Unity project

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the offset to be above and behind the plane
+        offset = new Vector3(30, 0, 10); // Previously no initialization of offset
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(speed * Time.deltaTime * Vector3.forward); // Previously Vector3.back without Time.deltaTime
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(-1 * rotationSpeed * Time.deltaTime * verticalInput * Vector3.right); // Previously without the verticalInput variable
+    }
+}
